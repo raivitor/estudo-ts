5cf440c17ef047107e64b49a15b14ed848fd99d8
Rai Vitor <raivitor@gmail.com>
2019-09-09 22:22:47 -0300

    Refactor prop negociacao.

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,13 +1,10 @@
 class Negociacao {
   // declaração das propriedades de classe
-  private _data;
-  private _quantidade;
-  private _valor;
-  constructor(data, quantidade, valor) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
+  constructor(
+    private _data: Date,
+    private _quantidade: number,
+    private _valor: number
+  ) {}
 
   get data() {
     return this._data;
