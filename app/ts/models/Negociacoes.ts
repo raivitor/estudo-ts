56fc4c698251b5244c1e5a0de43589681f8a4981
Rai Vitor <raivitor@gmail.com>
2019-09-09 22:39:21 -0300

    Added new model negociacoes.

---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,10 @@
+class Negociacoes {
+  private _negociacoes: Negociacao[] = [];
+  adiciona(negociacao: Negociacao) {
+    this._negociacoes.push(negociacao);
+  }
+
+  paraArray() {
+    return this._negociacoes;
+  }
+}
