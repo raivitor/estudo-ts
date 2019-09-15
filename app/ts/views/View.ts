e38da44af53bea4359118716a4919d9b66520ed2
Rai Vitor <raivitor@gmail.com>
2019-09-15 14:40:42 -0300

    Improve generic class.

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,15 @@
-class View {
+class View<T> {
   protected _elemento: Element;
 
   constructor(selector: string) {
     this._elemento = document.querySelector(selector);
+  }
+
+  update(model: T): void {
+    this._elemento.innerHTML = this.template(model);
+  }
+
+  template(model: T): string {
+    throw new Error("Precisa implementar esse método");
   }
 }
